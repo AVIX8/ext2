fcffca44e7d7e4e9939eb3e6407eaedadc7781c4
AVIX <avix256@gmail.com>
2022-05-27 14:09:54 +0700

    добавил начальную страницу

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>index</div>
+<v-app>
+
+  <v-row align="center" justify="center" class="ma-0">
+    <v-card class="">
+      <v-card-text>
+
+
+        <v-file-input
+          :value="file"
+          prepend-icon="mdi-file"
+          accept=".ext2volume"
+          placeholder="открыть .ext2volume"
+        />
+        <v-btn  @click="createNewVolume"> Создать ext2volume</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-row>
+  </v-app>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      file: undefined,
+    }
+  },
+  methods: {
+    createNewVolume() {
+      console.log('createNewVolume')
+    },
+  },
 }
 </script>
